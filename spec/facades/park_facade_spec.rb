9d8a2d517f93294a17cdfd301db3ad95109d268f
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-05 14:04:46 -0400

    all user stories complete

--- spec/facades/park_facade_spec.rb
+++ spec/facades/park_facade_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe ParkFacade do
   describe '#get_parks' do
     it 'returns the parks as an array' do
       parks = ParkFacade.get_parks('tn')
-      require 'pry'; binding.pry 
       expect(parks.first.name).to eq("Andrew Johnson National Historic Site")
       expect(parks).to be_an(Array)
     end
